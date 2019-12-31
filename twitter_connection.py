b5f90daaa0060869980813d0458d2d4dc43ee52c
Jaime Escobar <jjescobar@uninorte.edu.co>
2019-12-31 14:52:18 -0500

    typo

--- twitter_connection.py
+++ twitter_connection.py
@@ -5,6 +5,6 @@ def create_api_connection() -> twitter.Api():
     api = twitter.Api(consumer_key=os.environ['TWITTER_API_KEY'],
                     consumer_secret=os.environ['TWITTER_API_SECRET'],
                     access_token_key=os.environ['TWITTER_ACCESS_TOKEN'],
-                    access_token_secret=os.environ['TWITTER_ACCESS_TOKEN_SECERT'])
+                    access_token_secret=os.environ['TWITTER_ACCESS_TOKEN_SECRET'])
 
     return api
